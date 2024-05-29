43cfcf6c8cc75698703ddefbfc7898a4d0270ba2
justindstein <jaydstein@gmail.com>
2024-05-29 15:40:45 -0300

    Add HomeLocation component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @Component({
   standalone: true,
@@ -8,9 +10,16 @@ import { Component } from '@angular/core';
     <header class="brand-name">
       <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
     </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
   </main>
   `,
   styleUrls: ['./app.component.css'],
+  imports: [HomeComponent, HousingLocationComponent],
 })
 export class AppComponent {
   title = 'homes';
